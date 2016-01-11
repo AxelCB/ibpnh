0e6fe155dd91fcfa826a8e3209fc13cd9db4d29d
AxelCB <axcbovy@gmail.com>
2016-01-11 15:40:25 -0300

    starting with semantic ui

---
+++ src/org/kairos/ibpnh/model/devotional/BibleStudy.java
@@ -0,0 +1,11 @@
+package org.kairos.ibpnh.model.devotional;
+
+/**
+ * Created by acollardbovy on 06/11/2015.
+ */
+public class BibleStudy {
+
+    private String title;
+
+    private String body;
+}
