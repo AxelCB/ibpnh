2c694fa5477350fd5836c294986da0124b35c868
Axel Collard Bovy <axcbovy@gmail.com>
2016-04-01 10:31:07 -0300

    Más inserts BBDD

--- core/resources/BBDDInit.sql
+++ core/resources/BBDDInit.sql
@@ -40,19 +40,30 @@ VALUES (nextval('parameter_seq'), FALSE, 'Tag de Lenguaje para locales', TRUE, T
 INSERT INTO parameter (id, deleted, description, fixed, global, name, type, value, tags,viewed)
 VALUES (nextval('parameter_seq'), FALSE, 'Habilitación de registración de usuarios por la web', TRUE, TRUE, 'user.registration', 'BOOLEAN','false','user, registration,enabled',FALSE);
 
+INSERT INTO parameter (id, deleted, description, fixed, global, name, type, value, tags,viewed)
+VALUES (nextval('parameter_seq'), FALSE, 'Costo del algoritmo de hashing de contraseñas', TRUE, TRUE, 'hash.cost', 'LONG','10','password,hash,cost',FALSE);
+
+INSERT INTO parameter (id, deleted, description, fixed, global, name, type, value, tags,viewed)
+VALUES (nextval('parameter_seq'), FALSE, 'Cantidad de intentos de inicio de sesión antes de bloqueo', TRUE, TRUE, 'login.max.attempts', 'LONG','10','login,max,attempts',FALSE);
+
+INSERT INTO roletype (id, deleted, description, name, passwordreseter_id, roletypeenum) VALUES ( 1, false, 'Administrador general del sistema', 'Administrador', null, 0);
+
+INSERT INTO role (id,deleted,roletype_id) VALUES (1,FALSE,1);
+
 INSERT INTO "user"(id, deleted, disabledcause, enabled, enablinghash, firstlogin, hashcost, loginattempts, password, username, role_id)
-    VALUES (nextval('user_seq'), FALSE, NULL, TRUE,NULL,FALSE,1,0,'$2a$08$QQwZQMLfpH7IxUZOXlI8iOOrLXu9/GiW/W73a939jkqRiKi.hmV5y', admin,1);
+    VALUES (nextval('user_seq'), FALSE, NULL, TRUE,NULL,FALSE,1,0,'$2a$08$QQwZQMLfpH7IxUZOXlI8iOOrLXu9/GiW/W73a939jkqRiKi.hmV5y', 'admin',1);
 
 --Function
-INSERT INTO function (id, actionname, deleted, description, menuname, name, submenuname, uri)
-  VALUES (nextval('function_seq'), 'methodParkingEnd', FALSE, 'MethodParkingEnd', 'graphics', 'methodParkingEnd', NULL, '/graphic/methodParkingEnd');
-  --Function
-INSERT INTO function (id, actionname, deleted, description, menuname, name, submenuname, uri)
-  VALUES (nextval('function_seq'), 'methodParkingEnd', FALSE, 'MethodParkingEnd', 'graphics', 'methodParkingEnd', NULL, '/graphic/methodParkingEnd');
+INSERT INTO function (id, actionname, deleted, description, menuname, name, submenuname, uri) VALUES (nextval('function_seq'), 'createDocumentType', FALSE, 'CreateDocumentType', 'configuration', 'createDocumentType', NULL, '/documentType/create');
 -- Role types function
 INSERT INTO roletypefunction (id, deleted, disabledcause, enabled, function_id, roletype_id) VALUES (nextval('roletypefunction_seq'), false, NULL, true, currval('function_seq'), 1);
 -- Role types
 INSERT INTO rolefunction (id, deleted, disabledcause, enabled, function_id, role_id)SELECT nextval('rolefunction_seq'), false, NULL, true, currval('function_seq'), id_role_type.id FROM (SELECT distinct(r.id) FROM role r WHERE r.roletype_id = 1) as id_role_type;
 
-
+--Function
+INSERT INTO function (id, actionname, deleted, description, menuname, name, submenuname, uri) VALUES (nextval('function_seq'), 'createDailyDevotional', FALSE, 'CreateDailyDevotional', 'devotional', 'createDailyDevotional', NULL, '/dailyDevotional/create');
+-- Role types function
+INSERT INTO roletypefunction (id, deleted, disabledcause, enabled, function_id, roletype_id) VALUES (nextval('roletypefunction_seq'), false, NULL, true, currval('function_seq'), 1);
+-- Role types
+INSERT INTO rolefunction (id, deleted, disabledcause, enabled, function_id, role_id)SELECT nextval('rolefunction_seq'), false, NULL, true, currval('function_seq'), id_role_type.id FROM (SELECT distinct(r.id) FROM role r WHERE r.roletype_id = 1) as id_role_type;
 
