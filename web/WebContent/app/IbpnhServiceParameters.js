e1183e2934303211badb823e3ecb18e927a99872
Axel Collard Bovy <axcbovy@gmail.com>
2018-05-08 00:00:05 -0300

    Clean up old project

--- web/WebContent/app/IbpnhServiceParameters.js
+++
@@ -1,5 +0,0 @@
-//the only thing this does is define the URL in a global variable
-var ibpnhServiceUrl = (location.origin)?location.origin+"/core":location.protocol + "//" + location.host+"/core";
-var supported = ['es'];
-var defaultLang = 'es';
-var canSubscribe = false;
